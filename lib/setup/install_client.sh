09d9315ffea4f482f21f3553adcc120ebc56b92e
Robin Lennox <robinlennox@users.noreply.github.com>
2016-12-06 22:52:46 +1300

    Added: Missing build package for Iodine.

--- lib/setup/install_client.sh
+++ lib/setup/install_client.sh
@@ -34,7 +34,7 @@ function setup {
     
     # Install PKG #
     export LC_ALL=C
-    for installpkg in git build-essential python-pip python-scapy tcpdump dnsutils wireless-tools wpasupplicant nmap ssh nload htop traceroute ntpdate
+    for installpkg in git build-essential python-pip python-scapy tcpdump dnsutils wireless-tools wpasupplicant nmap ssh nload htop traceroute ntpdate zlib1g-dev
     do
         checkinstalled=$(dpkg-query -l | grep ${installpkg})
         if [ "" == "${checkinstalled}" ]; then
