b35237bcb3e26d5078165bd78cce42896ca42ccd
Robin Lennox <robinlennox@users.noreply.github.com>
2017-03-07 18:00:21 +1300

    Added Masscan.

--- lib/setup/install_client.sh
+++ lib/setup/install_client.sh
@@ -41,7 +41,7 @@ function setup {
     
     # Install PKG #
     export LC_ALL=C
-    for installpkg in git build-essential python-pip python-scapy tcpdump dnsutils wireless-tools wpasupplicant nmap ssh nload htop traceroute ntpdate zlib1g-dev screen rfkill
+    for installpkg in git build-essential python-pip python-scapy tcpdump dnsutils wireless-tools wpasupplicant nmap ssh nload htop traceroute ntpdate zlib1g-dev screen rfkill masscan
     do
         checkinstalled=$(dpkg-query -l | grep ${installpkg})
         if [ "" == "${checkinstalled}" ]; then
